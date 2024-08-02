b58a6992790f737d7bfdf627d7238fa0c4466e72
khoubaieb maamouri <83957252+khoubaib-sudo@users.noreply.github.com>
2024-08-02 14:39:48 +0200

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='head-text'>react app</h1>
+    </div>
+  )
+}
+
+export default App
